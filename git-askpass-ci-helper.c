317782f8841576d1cd130e160d348be03f1ec28b
Daniel Komisar <dkomisar@anaconda.com>
2022-09-16 14:12:24 -0400

    copy from worker

--- git-askpass-ci-helper.c
+++ git-askpass-ci-helper.c
@@ -1,7 +1,16 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 int main(int argc, char** argv)
 {
-    printf("Hello world\n");
+    const char * var_name = getenv("GIT_ASKPASS_ENVIRONMENT_VARIABLE");
+    if (!var_name)
+        var_name = "CONDA_CLONE_SECRET";
+
+    const char * secret_value = getenv(var_name);
+    if (!secret_value)
+        return 1;
+    
+    printf("%s\n", secret_value);
     return 0;
 }
