ecbd69d4adea2cfb138a4cdaff9b73b758152220
Daniel Komisar <dkomisar@anaconda.com>
2022-09-19 18:37:05 -0400

    updates

--- git-askpass-ci-helper.cxx
+++ git-askpass-ci-helper.cxx
@@ -6,51 +6,42 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    ofstream log_file("git-askpass-ci-helper.log", ios::out | ios::trunc);
-    ostream* log = &log_file;
+    ofstream log("git-askpass-ci-helper.log", ios::out | ios::trunc);
 
-    const char* debug = getenv("GIT_ASKPASS_DEBUG");
-    if (debug)
-    {
-        log = &cout;
-    }
-
+    // get variable name which has secret
     const char * var_name_p = getenv("GIT_ASKPASS_ENVIRONMENT_VARIABLE");
     if (! var_name_p)
     {
-        *log << "GIT_ASKPASS_ENVIRONMENT_VARIABLE not set" << endl;
+        log << "GIT_ASKPASS_ENVIRONMENT_VARIABLE not set" << endl;
         return 1;
     }
     string var_name(var_name_p);
     if (var_name.empty())
     {
-        *log << "GIT_ASKPASS_ENVIRONEMTN_VARIABLE is empty string" << endl;
+        log << "GIT_ASKPASS_ENVIRONEMTN_VARIABLE is empty string" << endl;
         return 2;
     }
-    if (var_name.length() > 500)
+    if (var_name.length() > 100)
     {
-        *log << "GIT_ASKPASS_ENVIRONMENT_VARIABLE is too long " << var_name.length() << endl;
+        log << "GIT_ASKPASS_ENVIRONMENT_VARIABLE is too long " << var_name.length() << endl;
         return 3;
     }
-         
+
+    // get secret 
     const char * secret_value_p = getenv(var_name.c_str());
     if (! secret_value_p)
     {
-        *log << "secret value not set" << endl;
+        log << "secret value not set" << endl;
         return 4;
     }
     string secret_value(secret_value_p);
-    if (secret_value.empty())
+    if (secret_value.length() != 40)
     {
-        *log << "secret value is empty" << endl;
-        return 5;
-    }
-    if (secret_value.length() > 400)
-    {
-        *log << "secret value is too long" << endl;
+        log << "secret value is too long" << endl;
         return 6;
     }
 
+    // print secret
     cout << secret_value << endl; 
     return 0;
 }
